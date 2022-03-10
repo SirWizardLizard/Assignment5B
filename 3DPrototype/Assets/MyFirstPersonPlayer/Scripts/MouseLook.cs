183da92dd70dc19a0286d6b9f7629ce0bf7b32f7
Zechariah Burrus <zechburrus@gmail.com>
2022-03-10 15:57:15 -0600

    almost finished

---
+++ 3DPrototype/Assets/MyFirstPersonPlayer/Scripts/MouseLook.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour {
+    public float mouseSensitivity = 100f;
+    public GameObject player;
+    private float verticalLookRotation = 0f;
+
+    // Update is called once per frame
+    void Update() {
+        //Mouse inpuit adn assign it to floats
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        //Rotates the player with horizontal mouse input
+        player.transform.Rotate(Vector3.up * mouseX);
+
+        //Rotate the camera around the x axis with vertical mouse input
+        verticalLookRotation -= mouseY;
+        //Clamps rotation so the player doesn't over-rotate and look behind themselves upside down
+        verticalLookRotation = Mathf.Clamp(verticalLookRotation, -90f, 90f);
+        transform.localRotation = Quaternion.Euler(verticalLookRotation, 0f, 0f);
+    }
+
+    private void OnApplicationFocus(bool focus) {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+}
---
+++ 3DPrototype/Assets/MyFirstPersonPlayer/Scripts/ShootWithRaycasts.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootWithRaycasts : MonoBehaviour {
+    public float damage = 10f;
+    public float range = 100f;
+    public float hitForce = 15f;
+    public Camera cam;
+
+    public ParticleSystem muzzleFlash;
+
+    // Update is called once per frame
+    void Update() {
+        if(Input.GetButtonDown("Fire1")) { Shoot(); }
+    }
+
+    void Shoot() {
+        muzzleFlash.Play();
+
+        RaycastHit hitInfo;
+        if(Physics.Raycast(cam.transform.position, cam.transform.forward, out hitInfo, range)) {
+            Debug.Log(hitInfo.transform.gameObject.name);
+
+            Target target = hitInfo.transform.gameObject.GetComponent<Target>();
+            if(target != null) {
+                target.TakeDamage(damage);
+                Vector3 targetScale = hitInfo.transform.localScale;
+                hitInfo.transform.localScale = new Vector3(targetScale.x - 1, targetScale.y - 1, targetScale.z - 1);
+            }
+
+            if(hitInfo.rigidbody != null) {
+                hitInfo.rigidbody.AddForce(cam.transform.TransformDirection(Vector3.forward) * hitForce, ForceMode.Impulse);
+            }
+        }
+    }
+}
